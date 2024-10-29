50e597d0af8bd9334fe77ce40fcf8b01d5e93f0e
Ana Laura Marques <lauramarques181@gmail.com>
2024-10-28 21:43:06 -0300

    add new project

--- santander-coders/santander-coders/src/main/java/br/com/santander/santander_coders/model/SantanderCodersApplication.java
+++ santander-coders/santander-coders/src/main/java/br/com/santander/santander_coders/model/SantanderCodersApplication.java
@@ -1,13 +1,10 @@
-package br.com.santander.santander_coders;
-
+package br.com.santander.santander_coders.model;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SantanderCodersApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SantanderCodersApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SantanderCodersApplication.class, args);
+    }
 }
